0220808a501cf9c85e36ef758214cc7a6085819f
Nelson Jaramillo <elm4st3r3000@gmail.com>
2025-10-20 16:42:46 -0600

    Fix: eliminación de duplicado de PORT

--- index.js
+++ index.js
@@ -548,6 +548,8 @@ app.get("/", (req, res) => {
   res.send("✅ Servidor Amora Live está funcionando correctamente.");
 });
 
+const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
-  console.log(`✅ Amora Live server running on port ${PORT}`);
+  console.log(`✅ Servidor Amora Live ejecutándose en el puerto ${PORT}`);
 });
